d402b0368b03ee46355df80d840759e5fa343a35
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-10-27 12:29:42 +0200

    Updates

---
+++ precache-manifest.aacf13a5116a556b83d44e91737244a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ae908533fbab551049463e691b306c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7330077145d62457718",
+    "url": "./static/css/main.7071c46e.chunk.css"
+  },
+  {
+    "revision": "697c898a2b5aff7738fc",
+    "url": "./static/js/2.d1edb005.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.d1edb005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7330077145d62457718",
+    "url": "./static/js/main.f3f8b760.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
